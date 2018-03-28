0f7fe7310d54780f0b0889eecda4fa48505272d8
Gandhi Gannamaneni <gandhi.gannamaneni@kony.com>
2018-03-28 10:56:28 +0530

    adding files

---
+++ TmbVmAppDelegate.m
@@ -0,0 +1,26 @@
+
+#import "TmbVmAppDelegate.h"
+#import <InsertFramework/InsertFramework.h>
+
+@implementation TmbVmAppDelegate
+
+- (BOOL)application:(UIApplication *)app
+            openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options;
+{
+    if ([[url scheme] containsString:@"insert"]) {
+        [[InsertManager sharedManager] initWithUrl:url];
+        return YES;
+    }
+    return YES;
+}
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions;{
+    [[InsertManager sharedManager] initSDK:@"15923feb76f7f8a671257dc0a17599839245bfa2617d1a1bf482cc693ea1473edf50299a3487027c604bf5e24e63fabef7bbd2c2ca1b39a39c8526a001fbf6932b132e7a4e67e13789ed5999908d1962.082b886765522de64c6f6427e1f03735.27ccd507135cd66cd9671667038f46e54fde5d38fc27cde2fb7498d9f5cc67b4"
+                               companyName: @"tmbbankthailand"];
+    [super application:application didFinishLaunchingWithOptions:launchOptions];
+    return YES;
+}
+
+@end
+
---
+++ TransferHandler.h
@@ -0,0 +1,17 @@
+//
+//  TransferHandler.h
+//  PaymentsExtn
+//
+//  Created by Gandhi Gannamaneni on 9/14/17.
+//
+
+#ifndef TransferHandler_h
+#define TransferHandler_h
+
+#import <Intents/Intents.h>
+
+@interface TransferHandler : INExtension
+
+@end
+
+#endif /* TransferHandler_h */
